538b77028a34ffa333a5b8792e9dd4e1100eb117
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-10-19 10:45:16 +0200

    Intégration des utilisateurs

---
+++ src/Entity/User.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="l'adresse email {{ value }} existe déjà."
+ * )
+ * @UniqueEntity(
+ *     fields={"username"},
+ *     message="Un autre utilisateur est déjà enregistré avec ce nom d'utilisateur {{ value }}."
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Votre mot de passe doit contenir au minimum 8 caractères")
+     */
+    private $hash;
+
+    /**
+     * @Assert\EqualTo(propertyPath="hash", message="Vous n'avez pas tapé le même mot de passe")
+     */
+    private $confirmPassword;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    public function getConfirmPassword(): ?string
+    {
+        return $this->confirmPassword;
+    }
+
+    public function setConfirmPassword(string $confirmPassword): self
+    {
+        $this->confirmPassword = $confirmPassword;
+
+        return $this;
+    }
+
+    public function getRoles(){
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword(){
+        return $this->password = $this->hash;
+    }
+
+    public function getSalt(){}
+
+    public function eraseCredentials(){}
+
+}
